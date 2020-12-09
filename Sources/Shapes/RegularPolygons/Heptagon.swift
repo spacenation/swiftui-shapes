aa8d4be9a9a07d87a4a43744eda30050ddc5f5b8
Todd George Kelly <toddgkelly@me.com>
2020-12-09 00:50:34 -0700

    Curved Regular Polygons
    Added 'radius' to all the regular polygons to make it so we can have curved corners.

--- Sources/Shapes/RegularPolygons/Heptagon.swift
+++ Sources/Shapes/RegularPolygons/Heptagon.swift
@@ -2,24 +2,32 @@ import SwiftUI
 
 public struct Heptagon: InsettableShape {
     let inset: CGFloat
+	let radius: CGFloat
     
     public func inset(by amount: CGFloat) -> Heptagon {
-        Heptagon(inset: self.inset + amount)
+        Heptagon(inset: self.inset + amount, radius: radius)
     }
     
     public func path(in rect: CGRect) -> Path {
-        Path.regularPolygon(sides: 7, in: rect, inset: inset)
+        Path.regularPolygon(sides: 7, in: rect, inset: inset, radius: radius)
     }
     
-    public init() {
-        inset = 0
-    }
+	public init() {
+		inset = 0
+		radius = 0
+	}
+
+	public init(radius: CGFloat) {
+		self.inset = 0
+		self.radius = radius
+	}
 }
 
 extension Heptagon {
-    init(inset: CGFloat) {
-        self.inset = inset
-    }
+	init(inset: CGFloat, radius: CGFloat) {
+		self.inset = inset
+		self.radius = radius
+	}
 }
 
 struct Heptagon_Previews: PreviewProvider {
--- Sources/Shapes/RegularPolygons/RegularPolygon.swift
+++ Sources/Shapes/RegularPolygons/RegularPolygon.swift
@@ -3,48 +3,52 @@ import SwiftUI
 public struct RegularPolygon: InsettableShape {
     let sides: Int
     let inset: CGFloat
+	let radius: CGFloat
     
     public func inset(by amount: CGFloat) -> RegularPolygon {
-        RegularPolygon(sides: self.sides, inset: self.inset + amount)
+        RegularPolygon(sides: self.sides, inset: self.inset + amount, radius: radius)
     }
     
     public func path(in rect: CGRect) -> Path {
-        Path.regularPolygon(sides: self.sides, in: rect, inset: inset)
+        Path.regularPolygon(sides: self.sides, in: rect, inset: inset, radius: radius)
     }
     
-    public init(sides: Int) {
+	public init(sides: Int, radius: CGFloat = 0) {
         self.sides = sides
         self.inset = 0
+		self.radius = radius
     }
 
-    public init(sides: Double) {
+	public init(sides: Double, radius: CGFloat = 0) {
         self.sides = Int(sides.rounded(.down))
         self.inset = 0
+		self.radius = radius
     }
 }
 
 extension RegularPolygon {
-    init(sides: Int, inset: CGFloat) {
+	init(sides: Int, inset: CGFloat, radius: CGFloat = 0) {
         self.sides = sides
         self.inset = inset
+		self.radius = radius
     }
 }
 
 struct RegularPolygon_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            RegularPolygon(sides: 4)
+            RegularPolygon(sides: 4, radius: 5)
                 .strokeBorder(lineWidth: 20)
                 .foregroundColor(.blue)
             
-            Pentagon()
+            Pentagon(radius: 5)
                 .strokeBorder(lineWidth: 20)
                 .foregroundColor(.yellow)
             
             Hexagon()
                 .foregroundColor(.orange)
             
-            Heptagon()
+            Heptagon(radius: 5)
                 .foregroundColor(.blue)
             
             Octagon()
